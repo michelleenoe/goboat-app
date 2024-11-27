2c45f9786dab6f9e57157633d53bd81fc6d6e528
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-11-27 15:42:46 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,35 @@
-/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      scrollSnapType: {
+        x: "x mandatory",
+      },
+      scrollSnapAlign: {
+        center: "center",
+      },
+      fontFamily: {
+        sans: ['Faro', ...fontFamily.sans],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: "var(--color-white)",
+        black: "var(--color-black)",
+        dark: "var(--color-dark)",
+        light: "var(--color-light)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        error: "var(--color-error)",
+      },
+      spacing: {
+        none: "0px",
+        small: "8px",
+        medium: "16px",
+        large: "24px",
+        xlarge: "32px",
       },
     },
   },
