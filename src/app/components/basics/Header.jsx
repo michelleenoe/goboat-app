d855a350eb7caa23f17dff2af5109ca3eb17c43e
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-12-03 13:54:13 +0100

    header

---
+++ src/app/components/basics/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full py-4 flex justify-left">
+      <img src="/icons/gb-logo.svg" alt="Logo" className="h-10" />
+    </header>
+  );
+}
