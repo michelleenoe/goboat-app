d3f2c9066fa4ba7844d5f262df7de24bd66e268d
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-12-03 11:54:10 +0100

    removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      scrollSnapType: {
-        x: "x mandatory",
-      },
-      scrollSnapAlign: {
-        center: "center",
-      },
       fontFamily: {
         sans: ['Faro', ...fontFamily.sans],
       },
@@ -23,13 +17,6 @@ module.exports = {
         primary: "var(--color-primary)",
         secondary: "var(--color-secondary)",
         error: "var(--color-error)",
-      },
-      spacing: {
-        none: "0px",
-        small: "8px",
-        medium: "16px",
-        large: "24px",
-        xlarge: "32px",
       },
     },
   },
