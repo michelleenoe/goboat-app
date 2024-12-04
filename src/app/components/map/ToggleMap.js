eea1f879709cc91d1c2ca41ca82f5f0ca0840acf
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-12-04 16:43:03 +0100

    edited aria

--- src/app/components/map/ToggleMap.js
+++ src/app/components/map/ToggleMap.js
@@ -4,7 +4,7 @@ export default function ToggleMapStyleButton({ toggleMapStyle }) {
     <button
       onClick={toggleMapStyle}
       className="bg-white p-3 rounded-full shadow-md"
-      aria-label="Toggle Map Style"
+      aria-label="Toggle map icon"
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" fill="currentColor">
         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 7h-2V6h-2v3H9v2h4v3h2v-3h2V9z"></path>
