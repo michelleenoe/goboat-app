0a32795b37aafd13b3deeef5e673fa76fe6ffbc8
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-12-05 12:07:47 +0100

    header fix

--- src/app/components/basics/Header.jsx
+++ src/app/components/basics/Header.jsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full py-4 mx-8 flex justify-left">
-      <Image
-        src="/icons/gb-logo.svg"
-        alt="Logo"
-        width={148} // Adjust the width as needed
-        height={34} // Adjust the height as needed
-        className="h-10"
-      />
+    <header className="w-full flex justify-left">
+      <div className="py-4 mx-8 ">
+        <Image
+          src="/icons/gb-logo.svg"
+          alt="Logo"
+          width={148} // Adjust the width as needed
+          height={34} // Adjust the height as needed
+          className="h-10"
+        />
+      </div>
     </header>
   );
 }
