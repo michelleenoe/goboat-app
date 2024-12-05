cc4024c529e62335c72cf116aeb0abd98a521612
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-12-05 12:01:04 +0100

    footer og header

--- src/app/components/basics/Footer.jsx
+++ src/app/components/basics/Footer.jsx
@@ -13,16 +13,18 @@ export default function Footer() {
 
   return (
     <>
-      <footer className="w-full bg-mediumBlue py-4 fixed bottom-0 left-0">
-        <div className="flex justify-around items-center">
-          <NavButton href="/" icon="/icons/home.svg" altText="Home" />
-          <NavButton href="/tools" icon="/icons/tool.svg" altText="Tools" />
-          <NavButton href="/map" icon="/icons/map.svg" altText="Map" />
-          <SettingsButton
-            onClick={openSettings}
-            icon="/icons/settings.svg"
-            altText="Settings"
-          />
+      <footer className="mt-10">
+        <div className="w-full bg-mediumBlue py-4 fixed bottom-0 left-0">
+          <div className="flex justify-around items-center ">
+            <NavButton href="/" icon="/icons/home.svg" altText="Home" />
+            <NavButton href="/tools" icon="/icons/tool.svg" altText="Tools" />
+            <NavButton href="/map" icon="/icons/map.svg" altText="Map" />
+            <SettingsButton
+              onClick={openSettings}
+              icon="/icons/settings.svg"
+              altText="Settings"
+            />
+          </div>
         </div>
       </footer>
 
--- src/app/components/basics/Header.jsx
+++ src/app/components/basics/Header.jsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <header className="w-full py-4 flex justify-left">
-      <img src="/icons/gb-logo.svg" alt="Logo" className="h-10" />
+    <header className="w-full py-4 mx-8 flex justify-left">
+      <Image
+        src="/icons/gb-logo.svg"
+        alt="Logo"
+        width={148} // Adjust the width as needed
+        height={34} // Adjust the height as needed
+        className="h-10"
+      />
     </header>
   );
 }
