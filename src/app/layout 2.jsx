583f89f45b8a9828e96d0b172832cd42bbcde1d5
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-12-05 11:58:01 +0100

    analytics

--- src/app/layout 2.jsx
+++ src/app/layout 2.jsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import "../styles/globals.css";
 export const metadata = {
   title: "GoBoat App",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
         <main className="pb-10">
           <div>{children}
           <SpeedInsights />
+          <Analytics />
           </div>
         </main>
       </body>
