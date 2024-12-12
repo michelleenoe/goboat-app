4692f32087712f16bda129202eddcc99dff278bd
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-12-12 11:49:02 +0100

    removed - function not used

--- src/app/lib/resetTimer.jsx
+++
@@ -1,8 +0,0 @@
-export const resetTimer = (newDuration) => {
-    if (typeof window !== "undefined") {
-      const totalTime = parseInt(newDuration, 10) * 60 * 60; 
-      localStorage.setItem("selectedDuration", newDuration);
-      localStorage.setItem("remainingTime", totalTime);
-    }
-  };
-  
