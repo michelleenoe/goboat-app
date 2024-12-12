648fac9f77062a2ac72549e278b5010999e207a7
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-12-12 13:55:50 +0100

    loading

--- src/app/LoadingPage.jsx
+++ src/app/LoadingPage.jsx
@@ -1,21 +1,13 @@
 "use client";
 import Image from "next/image";
-import { useTheme } from "@/contexts/ThemeContext";
 
 export default function LoadingPage() {
-  const { theme } = useTheme();
-
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <div className="text-center">
-        {/* Logo med zoom-animation */}
-        <div className="w-32 h-32 mx-auto relative animate-zoom">
+        <div className="w-32 h-32  bg-goboatBlue rounded-full  mx-auto relative animate-zoom">
           <Image
-            src={
-              theme === "dark"
-                ? "/Icons/Logo_darktheme.svg"
-                : "/Icons/Logo_lighttheme.svg"
-            }
+            src="/Icons/Logo_darktheme.svg"
             alt="GoBoat Logo"
             fill
             className="object-contain dark:hidden"
